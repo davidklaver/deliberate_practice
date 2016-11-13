cca00a9c1435b0a6d30954f5a3f654f867a5c2e4
davidklaver <davidjklaver@gmail.com>
2016-11-13 16:01:08 -0500

    add comments to two files

--- flatten.rb
+++ flatten.rb
@@ -1,6 +1,9 @@
+# flatten takes nested arrays and makes them one-dimensional
+
 a = [1,2,3,4,[5,6,7],8,9]
 a.flatten!
 p a
 
+# I suppose I could break it by misspelling it...
 a = [1,2,3,4,8,9]
 p a.flaten
--- take_method.rb
+++ take_method.rb
@@ -1,3 +1,7 @@
+# The "take" method takes an integer as an argument and returns the first (integer) elements from the array.
+
 a = [3,4,5,6,7]
 p a.take(3)
+
+# Does not work with negative numbers
 p a.take(-3)
